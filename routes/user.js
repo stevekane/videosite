9ff458eede51d994186b76d8b14a7cdd0b94e84a
Peter Chen <pchen@4-south.com>
2013-10-15 17:35:31 -0500

    added a user/edit route

--- routes/user.js
+++ routes/user.js
@@ -26,7 +26,8 @@ function createUser (req, res) {
       data.password = req.body.password;
       data.email = req.body.email;
       User.create(data, function (err, user) {
-        if (err) { return console.log(err); } 
+        if (err) { return res.status(400).send(err);
+        } 
         
         console.log(user.username, "created");
         return res.json(formatDbResponse(user));
@@ -35,17 +36,41 @@ function createUser (req, res) {
   });
 }
 
+function editUser(req, res){
+  updatedInfo = req.body;
+  delete updatedInfo._id;
+  delete updatedInfo.__v;
+  
+  //TODO: fix password hashing on modify, then remove this
+  delete updatedInfo.password;
+  
+  User.findOneAndUpdate({_id: req.body.id}, {$set: updatedInfo},
+    function (err, result) {
+      var response = {};
+      if (err) {
+        console.log(err);
+        res.json({message: err});
+      } else {
+        response['user'] = formatDbResponse(result);
+        res.send(response);
+      }
+    });
+
+};
+
+
 function login (req, res) {
-  return res.json({user: req.user});
+  return res.json({user: formatDbResponse(req.user)});
 }
 
 function logout (req, res) {
   req.logout();
-  return res.json({});
+  req.redirect('/');
 }
 
 exports.configure = function (app, passport, options) {
   app.post('/user/create', createUser);
   app.post('/user/login', passport.authenticate('local'), login);
-  app.get('/user/logout', logout);
+  app.all('/user/logout', logout);
+  app.post('/user/edit', verifyAuth, editUser);
 }
