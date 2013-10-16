52acb2c3669eb42b02bbef2b98f38ec858c4e061
Peter Chen <pchen@4-south.com>
2013-10-16 15:07:51 -0500

    login controller and template work

--- public/javascripts/controllers/Login.js
+++ public/javascripts/controllers/Login.js
@@ -1,25 +1,65 @@
+var set = Ember.set
+  , alias = Ember.computed.alias;
+
+var clearAndSetError = function (property, error) {
+  set(property, "value", "");
+  set(property, "error", error);
+};
+
+function loginUser(user, store){
+  store.load(App.User, user);
+};
+
 App.LoginController = Ember.Controller.extend({
-  username: null,
-  password: null,
+  needs: ['user'],
+  activeUser: alias('controllers.user.content'),
+  
+  loginHash: {
+    username: {value: "", error: ""},
+    password: {value: "", error: ""},
+  },
+  
+  resetFields: function (loginHash) {
+    set(loginHash, 'username.value', "");
+    set(loginHash, 'username.error', "");
+    set(loginHash, 'password.value', "");
+    set(loginHash, 'password.error', "");
+  },
+    
   actions:{
-    updateAccountInfo: function(){
-      user = this.get('username');
-      password = this.get('password');
-      _this = this;
-      $.ajax({
-        type: 'POST',
-        url: "http://localhost:3000/user/login",
-        data: {username:user, password: password},
-        success: function(response){
-          console.log("OK!", response);
-        },
-        error: function(response){
-          console.log(response);
-        },
-        complete: function(res){
-          _this.setProperties({username: null, password: null});
-        }
-      });
+  
+    attemptLogin: function(hash){
+        var passwordError
+          , store = this.get('store')
+          , self = this
+          , values = {
+          username: hash.username.value,
+          password: hash.password.value,
+        };
+        window.store2 = store;
+        
+        $.ajax({
+          type: 'POST',
+          url: "http://localhost:3000/user/login",
+          data: {username: values.username, 
+                 password: values.password
+                 },
+          success: function(response){
+            var user = response.user.user;
+            var emberUser = store.push(App.User, user);
+            self.set('activeUser', emberUser);
+            self.resetFields(hash);
+            self.transitionToRoute('index');
+          },
+          error: function(response){
+            console.log(response.responseText);
+            set(hash, "username.error", response.responseText);
+            set(hash, "password.error", response.responseText);
+          },
+          complete: function(res){
+            //self.resetFields(hash);
+          }
+        })
     }
   }
 });
