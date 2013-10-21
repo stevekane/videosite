73317854f082fa113cfb538b8e047f362cbc702a
steve kane <skane@4-south.com>
2013-10-21 17:05:20 -0500

    refactored many functions in user routes to be curried functions that resolve once all arguments are provided

--- routes/user.js
+++ routes/user.js
@@ -17,6 +17,7 @@ function checkForExistingUser (User, data) {
 }
 
 function checkForExistingUserById(User, id){
+  console.log('checkForExistingUserById');
   return callWithPromise(User, "findById", id);
 }
 
@@ -24,112 +25,78 @@ function checkForExistingUserById(User, id){
 //returns rejected promise if user else returns resolved promise
 function handleExistingUser (user) {
   console.log('handleExistingUser.  User -> ', user);
-
   if (user) { 
     throw new Error("User already exists!");
   }
   return user;
 }
 
+//not curried since inner function takes no parameters
 function createNewUser (User, data) {
-  return function (result) {
-    console.log('createNewUser', result);
+  return function () {
+    console.log('createNewUser');
     return callWithPromise(User, "create", data);
   }
 }
 
-function returnUser(res) {
-  return function (user) {
-    return res.json(formatUser(user));
-  }
-}
+var returnUser = _.curry(function (res, user) {
+  return res.json(formatUser(user));
+});
 
-function registerWithCustomerIO (cio) {
-  return function (user) {
-    console.log('registerWithCIO', user, "user");
-    cio.identify(user._id, user.email, {created_at: user.created_at});
-    return user;
-  }
-}
 
-function sendNewUserEmail(cio){
-  return function(user){
-    console.log('sending new user email')
-    cio.track(user.id, 'account_created', {
-      subscription_level: 'new_account'
-    });
-    return user;
-  }
-}
+var registerWithCustomerIO = _.curry(function (cio, user) {
+  console.log('registerWithCIO');
+  cio.identify(user._id, user.email, {created_at: user.created_at});
+  return user;
+});
 
-function sendUpdatedAccountInfoNotification(cio){
-  return function(user){
-    console.log('sending user email notifying acct change')
-    cio.track(user.id, 'email_changed')
-    return user;
-  }
-}
 
-function loginUser (req) {
-  return function (user) {
-    console.log('loginUser');
-    req.login(user, function (err) {
-      console.log("login error", err); 
-    });
-    return user;
-  }
-}
+var sendNewUserEmail = _.curry(function (cio, user) {
+  console.log('sending new user email')
+  cio.track(user.id, 'account_created', {
+    subscription_level: 'new_account'
+  });
+  return user;
+});
+
+var sendUpdatedAccountInfoNotification = _.curry(function (cio, user) {
+  console.log('sending user email notifying acct change')
+  cio.track(user.id, 'email_changed')
+  return user;
+});
+
+//We use a Q.defer here to allow us to throw or resolve the callback from login
+var loginUser = _.curry(function (req, user) {
+  console.log('loginUser');
+  var loginPromise = Q.defer();
+
+  req.login(user, function (err) {
+    if (err) {
+      loginPromise.reject(new Error("Login Unsuccessful."));
+    } else {
+      loginPromise.resolve(user); 
+    }
+  });
+  return loginPromise.promise;
+});
+
 
 //update and create w customer.io are actually the same
-function updateWithCustomerIO (cio) {
-  return function (user) {
-    console.log("user: ", user);
-    user.updated_at_timestamp("Edit Email");
-    cio.identify(user._id, user.email, {
-      updated_at: user.updated_at,
-      last_action: user.last_modified_action
-    });
-    return user;
-  }
-}
+var updateWithCustomerIO = _.curry(function (cio, user) {
+  console.log("user: ", user);
+  user.updated_at_timestamp("Edit Email");
+  cio.identify(user._id, user.email, {
+    updated_at: user.updated_at,
+    last_action: user.last_modified_action
+  });
+  return user;
+});
 
+//Not curried as inner function takes no params
 function editUserInfo(User, data){
-  return function(){
+  return function () {
     var updatedInfo = {email: data.email};
     return callWithPromise(User, "findOneAndUpdate", {_id: data.id}, {$set: updatedInfo});
-  }
-}
-
-function processNewUser (cio) {
-
-  return function (req, res) {
-    var data = req.body.user;
-
-    checkForExistingUser(User, {email: data.email})
-    .then(handleExistingUser)
-    .then(createNewUser(User, data))
-    .then(registerWithCustomerIO(cio))
-    .then(sendNewUserEmail(cio))
-    .then(loginUser(req))
-    .then(returnUser(res))
-    .fail(sendError(res))
-    .done();
-  }
-}
-
-function processEditUser (cio) {
-  return function (req, res) {
-    
-    var data = req.body.user;
-   
-    callWithPromise(User, "findOne", {email: data.email})
-    .then(handleExistingUser)
-    .then(editUserInfo(User, data))
-    .then(updateWithCustomerIO(cio))
-    .then(sendUpdatedAccountInfoNotification(cio))
-    .then(returnUser(res))
-    .fail(sendError(res))
-    .done();
   }
 }
 
@@ -158,6 +125,7 @@ function checkIfMatches(isMatch){
   return isMatch;
 }
 
+//not curried as the inner function takes no args
 function hashPassword (newPassword, salt) {
   console.log("hashpassword", newPassword, salt);
   return function () {
@@ -165,27 +133,11 @@ function hashPassword (newPassword, salt) {
   }
 }
 
-function updateUserPassword (id) {
-  return function (hash) {
-    return callWithPromise(User, "findOneAndUpdate", {_id: id}, {$set: {password: hash}}) 
-  }
-}
+var updateUserPassword =  _.curry(function (id, hash) {
+  console.log("updateUserPassword");
+  return callWithPromise(User, "findOneAndUpdate", {_id: id}, {$set: {password: hash}}) 
+});
 
-//TODO: SHOULD THE ANON FUNCTION HERE USE FORMAT BEFORE SENDING??
-function allowPasswordChange (req, res) {
-  var incomingPassword = req.body.oldpassword;
-  var newPassword = req.body.password;
-
-  comparePasswords(incomingPassword, req.user.password)
-  .then(checkIfMatches)
-  .then(hashPassword(newPassword, SALT_WORK_FACTOR))
-  .then(updateUserPassword(req.user._id))
-  .then(returnUser(res))
-  .fail(sendError(res))
-  .done();
-}
-
-//NOTE: THIS USES FORMAT RESPONSE WHICH IS SLIGHTLY DIFF THAN FORMATDBRESPONSE
 function restoreSession (req, res) {
   if (req.user && req.isAuthenticated()) {
     return res.status(200).json(formatUser(req.user)); 
@@ -219,34 +171,68 @@ var sendPasswordChangeNotification = _.curry(function (cio, newPassword, user){
   });
 });
 
-function processPasswordReset (cio) {
-  return function (req, res) {
-    var data = req.body;
-    checkForExistingUser(User, {email: data.email})
-    .then(handleInvalidUser)
-    .then(sendPasswordChangeRequest(cio))
-    .then(sendConfirmation(res))
-    .fail(sendError(res))
-    .done();
-  }
+var processNewUser = _.curry(function (cio, req, res) {
+  var data = req.body.user;
+
+  checkForExistingUser(User, {email: data.email})
+  .then(handleExistingUser)
+  .then(createNewUser(User, data))
+  .then(registerWithCustomerIO(cio))
+  .then(sendNewUserEmail(cio))
+  .then(loginUser(req))
+  .then(returnUser(res))
+  .fail(sendError(res))
+  .done();
+});
+
+var processEditUser = _.curry(function (cio, req, res) {
+  var data = req.body.user;
+ 
+  callWithPromise(User, "findOne", {email: data.email})
+  .then(handleExistingUser)
+  .then(editUserInfo(User, data))
+  .then(updateWithCustomerIO(cio))
+  .then(sendUpdatedAccountInfoNotification(cio))
+  .then(returnUser(res))
+  .fail(sendError(res))
+  .done();
+});
+
+function processPasswordChange (req, res) {
+  var incomingPassword = req.body.oldpassword;
+  var newPassword = req.body.password;
+
+  comparePasswords(incomingPassword, req.user.password)
+  .then(checkIfMatches)
+  .then(hashPassword(newPassword, SALT_WORK_FACTOR))
+  .then(updateUserPassword(req.user._id))
+  .then(returnUser(res))
+  .fail(sendError(res))
+  .done();
 }
 
-function processPasswordChange (cio) {
-  return function(req, res){
+var processPasswordReset = _.curry(function (cio, req, res) {
+  checkForExistingUser(User, {email: req.body.email})
+  .then(handleInvalidUser)
+  .then(sendPasswordChangeRequest(cio))
+  .then(sendConfirmation(res))
+  .fail(sendError(res))
+  .done();
+});
+
+var processPasswordChange = _.curry(function (cio, req, res) {
+  var userId = req.params.id;
+  var newPassword = Math.random().toString(36).slice(-8);
   
-    var userId = req.params.id;
-    var newPassword = Math.random().toString(36).slice(-8);
-    
-    checkForExistingUserById(User, userId)
-    .then(handleInvalidUser)
-    .then(hashPassword(newPassword, 10))
-    .then(updateUserPassword(userId))
-    .then(sendPasswordChangeNotification(cio, newPassword))
-    .then(sendConfirmation(res))
-    .fail(sendError(res))
-    .done();
-  }  
-}
+  checkForExistingUserById(User, userId)
+  .then(handleInvalidUser)
+  .then(hashPassword(newPassword, 10))
+  .then(updateUserPassword(userId))
+  .then(sendPasswordChangeNotification(cio, newPassword))
+  .then(sendConfirmation(res))
+  .fail(sendError(res))
+  .done();
+});
 
 
 exports.configure = function (app, passport, cio, options) {
@@ -257,10 +243,9 @@ exports.configure = function (app, passport, cio, options) {
   app.post('/user/authenticated', verifyAuth, confirmAuthentication); 
   app.get('/user/restore', restoreSession); 
   app.put('/user/edit', verifyAuth, processEditUser(cio));
-  app.post('/user/pwchange', verifyAuth, allowPasswordChange);
+  app.post('/user/pwchange', verifyAuth, processPasswordChange);
   app.post('/user/pwresetrequest', processPasswordReset(cio));
   app.get('/user/pwreset/:id', processPasswordChange(cio));
 
   return app;
-
 }
