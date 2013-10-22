2b9e95dc5cff88a19987820ee4b17a11c56f146c
steve kane <skane@4-south.com>
2013-10-22 14:28:49 -0500

    stored braintree locally.  Added initial spike for form components that are subclassable and contain common patterns/logic

--- public/javascripts/components/KaneFormComponent.js
+++ public/javascripts/components/KaneFormComponent.js
@@ -1,10 +1,11 @@
 var set = Ember.set
   , alias = Ember.computed.alias;
 
-function resetFields (hash) {
-  Object.keys(hash).forEach(function (key) {
-    set(hash, key, ""); 
-  });
+function setForDuration (context, key, value, duration) {
+  set(context, key, value);
+  Ember.run.later(context, function () {
+    set(context, key, ""); 
+  }, duration);
 }
 
 App.KaneFormComponent = Ember.Component.extend({
@@ -13,22 +14,98 @@ App.KaneFormComponent = Ember.Component.extend({
 
   disabled: false,
 
-  hash: {
-    main: "",
-    second: "lol"
+  hash: {},
+
+  errorDuration: 3000,
+
+  setForDuration: setForDuration,
+
+  verifications: [],
+
+  /*
+  It is recommended that your submitAction set disbaled true while
+  inflight and then back to false.  This will disable all inputs
+  in the form during flight
+  */
+  submitAction: Ember.K,
+
+  resetFields: function (hash) {
+    Object.keys(hash).forEach(function (key) {
+      set(hash, key, ""); 
+    });
+    return hash;
+  },
+
+  //return true if all verification fules pass
+  //else returns false and sets error to provided error 
+  clientSideVerify: function (hash, verifications) {
+    var self = this
+      , errorDuration = this.get('errorDuration');
+
+    var error = verifications.map(function (fn) {
+      return fn.call(self, hash); 
+    }).find(function (err) {
+      return (!!err); 
+    });
+
+    if (error) {
+      setForDuration(self, "error", error, errorDuration);
+      return false;
+    } else {
+      return true;
+    }
   },
 
   actions: {
 
-    submit: function (hash) {
-      console.log("submit has occurred");
-      set(this, "disabled", true);
-      Ember.run.later(this, function () {
-        set(this, "disabled", false); 
-      }, 2000);
+    submit: function () {
+      var self = this
+        , hash = this.get('hash')
+        , verifications = this.get('verifications')
+        , submitAction = this.get('submitAction');
+
+      if (this.clientSideVerify(hash, verifications)) {
+        submitAction.call(self, hash);
+      }
     },
-
-
   }
 
+});
+
+
+//check if the provided email contains an "@"
+function isValidEmail (hash) {
+  var error = (hash.email.indexOf("@") !== -1) 
+    ? "" 
+    : "Invalid email";
+  return error;
+}
+
+//method fired on form submission
+function attemptLogin (hash) {
+  var self = this;
+
+  set(self, "disabled", true);
+  Ember.$.ajax("/user/login", {type: "POST", data: hash})
+  .then(function (response) {
+    set(self, "disabled", false);
+    self.resetFields(hash);
+    self.sendAction("action", response.user);
+  })
+  .fail(function (response) {
+    self.resetFields(hash);
+    set(self, "disabled", false);
+    set(self, "error", "Login Failed");
+  });
+}
+
+App.KaneLoginFormComponent = App.KaneFormComponent.extend({
+  hash: {
+    email: "",
+    password: "",
+  },
+
+  verifications: [isValidEmail],
+
+  submitAction: attemptLogin
 });
