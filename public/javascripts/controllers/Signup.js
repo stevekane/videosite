cc542398543754d1ed3d5c834175a2f9dd9dffc9
steve kane <skane@4-south.com>
2013-10-22 19:29:31 -0500

    added signup form component and refactored appropriate controller and template to use it.  minor tweak to user/create endpoint

--- public/javascripts/controllers/Signup.js
+++ public/javascripts/controllers/Signup.js
@@ -7,49 +7,12 @@ App.SignupController = Ember.Controller.extend({
 
   activeUser: alias('controllers.user.content'),
 
-  error: "",
-
-  newAccountHash: {
-    email: "",
-    password: "",
-    confirmPassword: ""
-  },
-
-  resetFields: function (newAccountHash) {
-    set(newAccountHash, 'email', "");
-    set(newAccountHash, 'password', "");
-    set(newAccountHash, 'confirmPassword', "");
-  },
-
   actions: {
-    
-    signUp: function (hash) {
-      var passwordError
-        , store = this.get('store')
-        , self = this
-        , values = {
-        email: hash.email,
-        password: hash.password,
-      };
-      
-      if (hash.password !== hash.confirmPassword) {
-        set(self, "error", "Provided passwords did not match.");
-        set(hash, "password", "");
-        set(hash, "confirmPassword", "");
-        return;
-      } else {
-        store.createRecord('user', values)
-        .save()
-        .then(function (user) { 
-          self.set('activeUser', user);
-          self.resetFields(hash);
-          self.transitionToRoute('index');
-        })
-        .fail(function (error) {
-          console.log(error);
-          set(self, "error", JSON.parse(error.responseText)['error']);             
-        });
-      }
-    }
+    storeAndSetUser: function (user) {
+      var storedUser = this.get('store').push('user', user);
+      this.set('activeUser', storedUser);
+      this.transitionToRoute("index"); 
+    },
   }
+
 });
