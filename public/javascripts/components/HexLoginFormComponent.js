f0e1b21ec0b851295629a29ab3e7de73b307c080
steve kane <skane@4-south.com>
2013-10-26 16:26:29 -0500

    cleaned up Hexloginform and included comments

--- public/javascripts/components/HexLoginFormComponent.js
+++ public/javascripts/components/HexLoginFormComponent.js
@@ -7,39 +7,19 @@ var validateEmail = function (email) {
   }
 }
 
-var reportErrors = function (err) {
-  console.log(err.message);
-}
-
-var attemptSubmission = function (form) {
-  console.log("Submitting");
-}
-
-var badEmailForm = {
-  email: "smaugdoomsday.gov",
-  password: ""
-}
-
-var noPwForm = {
-  email: "smaugh@doomsday.gov",
-  password: ""
-}
-
-var goodForm = {
-  email: "smaugh@doomsday.gov",
-  password: "123456"
-}
-
-var validateFields = function (form) {
-  validateEmail(form.email); 
-}
-
 var validateForm = function (form) {
   if (form.email === "" || form.password === "") {
     throw new Error("Must provide both email and password"); 
   }
 }
 
+var validateFields = function (form) {
+  validateEmail(form.email); 
+}
+
+/*
+Local suite of functions for testing form lifecycle
+*/
 //test of local submit function
 var localSubmit = function (form) {
   return Ember.RSVP.Promise(function (resolve, reject) {
@@ -59,6 +39,15 @@ var localHandleFailure = function (error) {
   console.log(error.message);
 }
 
+//TESTS OF LOCAL STUFF
+//submit(this, badEmailForm, localSubmit, localHandleError, localHandleSuccess, localHandleFailure);
+//submit(this, noPwForm, localSubmit, localHandleError, localHandleSuccess, localHandleFailure);
+//submit(this, goodForm, localSubmit, localHandleError, localHandleSuccess, localHandleFailure);
+//
+
+
+//LOGIN FORM SUBMIT FUNCTION (generic really, could be partially applied)
+//With correct functions to provide better interface?
 //only assumption is that submitFn returns a thennable
 var submit = function (context, form, submitFn, errorFn, successFn, failFn) {
   try {
@@ -73,32 +62,30 @@ var submit = function (context, form, submitFn, errorFn, successFn, failFn) {
   .then(null, function (error) {failFn.call(context, error);});
 }
 
-//TESTS OF LOCAL STUFF
-//submit(this, badEmailForm, localSubmit, localHandleError, localHandleSuccess, localHandleFailure);
-//submit(this, noPwForm, localSubmit, localHandleError, localHandleSuccess, localHandleFailure);
-//submit(this, goodForm, localSubmit, localHandleError, localHandleSuccess, localHandleFailure);
-//
-
 //jquery implementation of submit
 var $submit = function (form) {
   return Ember.$.ajax("/user/login", {type: "POST", data: form});
 }
 
+//handleErrors w/ Ember
 var handleErrorEmber = function (error) {
   set(this, "disabled", false);
   set(this, "error", error.message);
 }
 
+//handle success w/ Ember
 var handleSuccessEmber = function (result) {
   set(this, "disabled", false);
   this.sendAction("action", result.user);
 }
 
+//handleFailure w/ Ember
 var handleFailureEmber = function (error) {
   set(this, "disabled", false);
-  set(this, "error", "Invalid Login Credentials.");
+  set(this, "error", "Invalid Login Credentials");
 }
 
+//Ember form component that uses our form functionality 
 App.HexLoginFormComponent = Ember.Component.extend({
   
   hash: {
@@ -109,8 +96,6 @@ App.HexLoginFormComponent = Ember.Component.extend({
   actions: {
     submit: function (hash) {
       set(this, "disabled", true);
-      //submit( this, hash, localSubmit, localHandleError, 
-      //        localHandleSuccess, localHandleFailure);
       submit(this, hash, $submit, handleErrorEmber, handleSuccessEmber, handleFailureEmber);
     }
   }
