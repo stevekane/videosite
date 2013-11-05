4ffdc76c36382fe01e735794c0943614cee21ba5
steve kane <skane@4-south.com>
2013-11-05 17:35:46 -0600

    complete rewrite of KaneFormComponent to use newly revamped form lib

--- public/javascripts/components/KaneFormComponent.js
+++ public/javascripts/components/KaneFormComponent.js
@@ -1,7 +1,4 @@
-var emailsMatch = Validations.fieldsMatch("newEmail", "confirmEmail")
-  , validateEmail = Validations.validateEmail("newEmail")
-  , checkIfBlank = Validations.checkIfBlank;
-
+var set = Ember.set;
 
 /*
 Bind values of fields hash to form input elements (or other mechanisms)
@@ -40,9 +37,35 @@ When the promise resolves, we will call "onSubmitSuccess", or "onSubmitError"
 
 */
 
+//set errors that apply to single fields
+function setFieldError (fields, results) {
+  results.forEach(function (result) {
+    set(fields, result.fields, result.error);
+  })
+}
+
+//set errors that apply to multiple fields
+function setFieldErrors (fields, results) {
+  results.forEach(function (result) {
+    result.fields.forEach(function (field) {
+      set(fields, field, result.error);
+    })
+  })
+}
+
 App.KaneFormComponent = Ember.Component.extend({
 
   fields: {},
+
+  disabled: false,
+
+  enableForm: function () {
+    this.set('disabled', false); 
+  },
+
+  disableForm: function () {
+    this.set('disabled', true); 
+  },
 
   localFieldValidations: [],
 
@@ -52,4 +75,57 @@ App.KaneFormComponent = Ember.Component.extend({
 
   remoteFormValidations: [],
 
+  setFieldError: setFieldError,
+
+  setFieldErrors: setFieldErrors,
+
+  //return a thennable from this
+  submitFn: function (hash) {
+    return Ember.RSVP.Promise(function (resolve, reject) {
+      resolve({});
+    });
+  },
+
+  successHandler: function (json) {},
+
+  failureHandler: function (err) {},
+
+  actions: {
+    submit: function () {
+      var self = this
+        , localFieldVals = this.get('localFieldValidations')
+        , localFormVals = this.get('localFormVals')
+        , remoteFieldVals = this.get('remoteFieldValidations')
+        , remoteFormVals = this.get('removeFormValidations')
+        , fields = this.get('fields');
+
+      //handle local field validations
+      var fieldResults = Forms.runLocalValidations(localFieldVals, fields);
+      var fieldErrors = Forms.checkForErrors(fieldResults);
+      if (fieldErrors) {
+        return this.setFieldError(fields, fieldErrors)
+      }
+
+      //handle local form validations
+      var formResults = Forms.runLocalValidations(localFormVals, fields);
+      var formErrors = Forms.checkForErrors(formResults);
+      if (formErrors) {
+        return this.setFieldErrors(fields, formErrors)
+      }
+
+      //handle remote field validation
+      //NOT IMPLEMENTED YET
+      //handle remote form validation
+      //NOT IMPLEMENTED YET
+      
+      //disable the form for remote roundtrip
+      self.disableForm();
+      //handle submission
+      self.submitFn(fields)
+      .then(self.successHandler)
+      .fail(self.failureHandler)
+      //re-enable the form regardless of outcome
+      .then(self.enableForm, self.enableForm);
+    }, 
+  }
 });
